501495f2d2cdcc165a92edf05a438afc0e7cef23
GrantODP <grantoranduprez@gmail.com>
2026-06-10 20:06:07 +0200

    Currently staying on pipewire backend

--- pvc_audio/src/lib.rs
+++ pvc_audio/src/lib.rs
@@ -1,9 +1,12 @@
 use crate::error::AudioError;
 
-pub mod audio;
+pub mod buffers;
+pub mod builder;
 pub mod device;
 pub mod error;
 pub mod stream;
+pub mod traits;
+pub mod util;
 
 pub type PvcAudioId = u32;
 
